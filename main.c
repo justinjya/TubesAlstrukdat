515b36c6ab875095f28810f30fbaa6c612fe9bd2
justinjya <103380665+justinjya@users.noreply.github.com>
2022-11-01 00:21:19 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include ".\src\console.c"
+
+int main()
+{
+    ArrayDin Games;
+    ArrayDin Queue;
+    ArrayDin History;
+    ArrayDin Test2;
+    Games = MakeArrayDin();
+    Queue = MakeArrayDin();
+    History = MakeArrayDin();
+
+    boolean endProgram = false;
+    char *command;
+
+    printf("Selamat datang pada BNMO\n");
+    while (endProgram == false)
+    {
+        printf("\nENTER COMMAND: ");
+        scanf("%s", command);
+        if (compareString(command, "START") == 1)
+        {
+            STARTBNMO(&Games);
+        }
+    }
+}
