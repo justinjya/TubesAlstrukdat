fcc277ffbbcb334021289d5d3923f3c28f324073
justinjya <103380665+justinjya@users.noreply.github.com>
2022-11-03 11:21:12 +0700

    Update mesinkarakterv2.h

--- src/ADT/mesinkarakterv2/mesinkarakterv2.h
+++ src/ADT/mesinkarakterv2/mesinkarakterv2.h
@@ -11,7 +11,7 @@
 extern char currentChar;
 extern boolean EOP;
 
-void START();
+void START(char *directory);
 /* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
    Karakter pertama yang ada pada pita posisinya adalah pada jendela.
    I.S. : sembarang
