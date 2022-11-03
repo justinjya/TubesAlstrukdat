27b088e8df8ee95ae37ac3b1f1211c7aa6807739
Richard Haris <88923240+r11i@users.noreply.github.com>
2022-11-03 13:00:25 +0700

    update mesinkarakterv2.h
    add type for mesinkarakter

--- src/ADT/mesinkarakterv2/mesinkarakterv2.h
+++ src/ADT/mesinkarakterv2/mesinkarakterv2.h
@@ -7,11 +7,12 @@
 #include "C:\Users\Legion\OneDrive - Institut Teknologi Bandung\vscode\C\tubes\src\boolean.h"
 
 #define MARK '.'
+#define MARK2 '\n'
 /* State Mesin */
 extern char currentChar;
 extern boolean EOP;
 
-void START(char *directory);
+void START(char *directory, int type);
 /* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
    Karakter pertama yang ada pada pita posisinya adalah pada jendela.
    I.S. : sembarang
@@ -19,7 +20,7 @@ void START(char *directory);
           Jika currentChar != MARK maka EOP akan padam (false)
           Jika currentChar = MARK maka EOP akan menyala (true) */
 
-void ADV();
+void ADV(int type);
 /* Pita dimajukan satu karakter.
    I.S. : Karakter pada jendela = currentChar, currentChar != MARK
    F.S. : currentChar adalah karakter berikutnya dari currentChar yang lama,
