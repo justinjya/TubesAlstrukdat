e5812dc3de24bb0a259c02224131254252fed96f
Richard Haris <88923240+r11i@users.noreply.github.com>
2022-11-03 13:32:06 +0700

    Update mesinkata.h

--- src/ADT/mesinkata/mesinkata.h
+++ src/ADT/mesinkata/mesinkata.h
@@ -20,10 +20,7 @@ typedef struct
 extern boolean EndWord;
 extern Word currentWord;
 
-/* type = 0 -> Untuk membaca karakter hingga newline. Kata dipisah per BLANK. Dipakai ketika menerima input stdin 
-   type = 1 -> Untuk membaca karakter hingga newline atau mark. Kata dipisah per newline. Dipakai ketika membaca pita dari file */
-
-void IgnoreBlanks();
+void IgnoreBlanks(int type);
 /* Mengabaikan satu atau beberapa BLANK
    I.S. : currentChar sembarang
    F.S. : currentChar ≠ BLANK atau currentChar = MARK */
@@ -32,14 +29,18 @@ void STARTWORD(char *directory, int type);
 /* I.S. : currentChar sembarang
    F.S. : EndWord = true, dan currentChar = MARK;
           atau EndWord = false, currentWord adalah kata yang sudah diakuisisi,
-          currentChar karakter pertama sesudah karakter terakhir kata */
+          currentChar karakter pertama sesudah karakter terakhir kata 
+    type = 0 -> Untuk membaca karakter hingga newline, dipakai ketika menerima input stdin 
+    type = 1 -> Untuk membaca karakter hingga newline atau mark, dipakai ketika membaca pita dari file */
 
 void ADVWORD(int type);
 /* I.S. : currentChar adalah karakter pertama kata yang akan diakuisisi
    F.S. : currentWord adalah kata terakhir yang sudah diakuisisi,
           currentChar adalah karakter pertama dari kata berikutnya, mungkin MARK
           Jika currentChar = MARK, EndWord = true.
-   Proses : Akuisisi kata menggunakan procedure SalinWord */
+   Proses : Akuisisi kata menggunakan procedure SalinWord
+   type = 0 -> Untuk membaca karakter hingga newline, dipakai ketika menerima input stdin 
+   type = 1 -> Untuk membaca karakter hingga newline atau mark, dipakai ketika membaca pita dari file */
 
 void CopyWord(int type);
 /* Mengakuisisi kata, menyimpan dalam currentWord
@@ -47,7 +48,9 @@ void CopyWord(int type);
    F.S. : currentWord berisi kata yang sudah diakuisisi;
           currentChar = BLANK atau currentChar = MARK;
           currentChar adalah karakter sesudah karakter terakhir yang diakuisisi.
-          Jika panjang kata melebihi NMax, maka sisa kata "dipotong" */
+          Jika panjang kata melebihi NMax, maka sisa kata "dipotong"
+    type = 0 -> Untuk membaca karakter hingga newline, dipakai ketika menerima input stdin 
+    type = 1 -> Untuk membaca karakter hingga newline atau mark, dipakai ketika membaca pita dari file */
 
 boolean isEndWord();
 
