ffb8953fe141a63244c1af816929d05c64aa6ef6
justinjya <103380665+justinjya@users.noreply.github.com>
2022-11-03 11:14:20 +0700

    Add files via upload

---
+++ src/ADT/mesinkata/mesinkata.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include "mesinkata.h"
+#include "C:\Users\Legion\OneDrive - Institut Teknologi Bandung\vscode\C\tubes\src\ADT\mesinkarakterv2\mesinkarakterv2.h"
+
+boolean EndWord;
+Word currentWord;
+
+void IgnoreBlanks()
+{
+    while (currentChar == ' ')
+    {
+        ADV();
+    }
+}
+
+void STARTWORD(char *directory, int type)
+{
+    if (type == 0)
+    {
+        START(NULL);
+        IgnoreBlanks();
+        if (currentChar == '\n')
+        {
+            EndWord = true;
+        }
+        else
+        {
+            EndWord = false;
+            CopyWord(type);
+        }
+    }
+    else
+    {
+        START(directory);
+        IgnoreBlanks();
+        if (currentChar == '\n' || currentChar == MARK)
+        {
+            EndWord = true;
+        }
+        else
+        {
+            EndWord = false;
+            CopyWord(type);
+        }
+    }
+}
+
+void ADVWORD(int type)
+{
+    if (type == 0)
+    {
+        ADV();
+        if (currentChar == '\n')
+        {
+            EndWord = true;
+        }
+        else
+        {
+            CopyWord(type);
+            IgnoreBlanks();
+        }
+    }
+    else
+    {
+        ADV();
+        if (currentChar == '\n' || currentChar == MARK)
+        {
+            EndWord = true;
+        }
+        else
+        {
+            CopyWord(type);
+        }
+    }
+}
+
+void CopyWord(int type)
+{
+    if (type == 0)
+    {
+        int i = 0;
+        while ((currentChar != '\n') && (currentChar != BLANK))
+        {
+            currentWord.TabWord[i] = currentChar;
+            ADV();
+            i++;
+        }
+        currentWord.TabWord[i] = '\n';
+
+        if (i > NMax)
+        {
+            currentWord.Length = NMax;
+        }
+        else
+        {
+            currentWord.Length = i;
+        }
+    }
+    else
+    {
+        int i = 0;
+        while (currentChar != MARK && currentChar != '\n')
+        {
+            currentWord.TabWord[i] = currentChar;
+            ADV();
+            i++;
+        }
+        currentWord.TabWord[i] = '\n';
+
+        if (i > NMax)
+        {
+            currentWord.Length = NMax;
+        }
+        else
+        {
+            currentWord.Length = i;
+        }
+    }
+}
+
+boolean isEndWord()
+{
+    return EndWord;
+}
