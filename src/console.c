fe5ba7ff49cf570943d149e011f60620e33bf75b
paiyahya <117044930+paiyahya@users.noreply.github.com>
2022-11-06 10:31:34 +0700

    Update console.c

--- src/console.c
+++ src/console.c
@@ -202,6 +202,7 @@ void LISTGAME (ArrayDin arrayGames){
     int i = 0;
     printf("Berikut adalah daftar game yang tersedia");
     
+    /* Menampilkan list game dari nomor 1 indeks ke-0 */
     for (i; i < arrayGames.Neff; i++){
         printf("%d. %s\n", i + 1, arrayGames.A[i]);
     }
