821e013173f3c4825b8e466616fba61ff72c93ad
justinjya <103380665+justinjya@users.noreply.github.com>
2022-11-09 21:34:06 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
-#include ".\src\console.c"
+#include ".\src\console.h"
 
 int main()
 {
+    system("cls");
     ArrayDin Games;
-    ArrayDin Queue;
-    ArrayDin History;
+    Queue GamesQueue;
     Games = MakeArrayDin();
-    Queue = MakeArrayDin();
-    History = MakeArrayDin();
+    CreateQueue(&GamesQueue);
 
     boolean endProgram = false;
     char *command;
+    command = (char *) malloc (50 * sizeof(char));
 
     printf("Selamat datang pada BNMO\n");
     printf("Jalankan command START atau LOAD <filename> untuk membuka file.\n");
@@ -18,6 +18,7 @@ int main()
     {
         printf("\nENTER COMMAND: ");
         inputString(0, command);
+        system("cls");
         if (IsEmpty(Games))
         {
             if (compareString(command, "START") == true)
@@ -30,11 +31,12 @@ int main()
                 inputfile = (char *) malloc (50 * sizeof(char));
                 ADVWORD(0);
                 wordToString(currentWord, inputfile);
-                LOADFILE(&Games, &History, inputfile);
+                LOADFILE(&Games, inputfile);
             }
             else
             {
                 printf("Belum ada file yang dibuka. Silahkan buka file terlebih dahulu.\n");
+                printf("Jalankan command START atau LOAD <filename> untuk membuka file.\n");
             }
         }
         else
@@ -46,6 +48,46 @@ int main()
                 ADVWORD(0);
                 wordToString(currentWord, inputfile);
                 SAVE(&Games, inputfile);
+            }
+            else if (compareString(command, "CREATEGAME") == true)
+            {
+                CREATEGAME(&Games);
+            }
+            else if (compareString(command, "LISTGAME") == true)
+            {
+                LISTGAME(&Games);
+            }
+            else if (compareString(command, "DELETEGAME") == true)
+            {
+                DELETEGAME(&Games);
+            }
+            else if (compareString(command, "QUEUEGAME") == true)
+            {
+                QUEUEGAME(&Games, &GamesQueue);
+            }
+            else if (compareString(command, "PLAYGAME") == true)
+            {
+                PLAYGAME(&Games, &GamesQueue);
+            }
+            else if (compareString(command, "SKIPGAME") == true)
+            {
+                ADVWORD(0);
+                int skips;
+                skips = wordToInteger(currentWord);
+                SKIPGAME(&Games, &GamesQueue, skips);
+            }
+            else if (compareString(command, "QUIT") == true)
+            {
+                QUIT(&Games, &GamesQueue);
+                endProgram = true;
+            }
+            else if (compareString(command, "HELP") == true)
+            {
+                HELP();
+            }
+            else
+            {
+                printf("Command tidak dikenali, silahkan masukkan command yang valid.\n");
             }
         }
     }
