8eeab13bf04abe8428e2fb1a02a307a051c34fb4
justinjya <103380665+justinjya@users.noreply.github.com>
2022-11-30 00:24:09 +0700

    Update arraydin_sb.c

--- src/ADT/scoreboard/arraydin_sb.c
+++ src/ADT/scoreboard/arraydin_sb.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 #include "arraydin_sb.h"
 
-ArrayDin_SB MakeArrayDin_SB()
+ArrayDin_SB MakeArrayDin_SB(int Neff)
 {
     ArrayDin_SB Arr;
     Arr.A = (ElType_SB *) malloc (InitialSize * sizeof(ElType_SB));
     Arr.Capacity = InitialSize;
-    Arr.Neff = 0;
+    Arr.Neff = Neff;
+    for (IdxType i = 0; i < Neff; i++)
+    {
+        CreateEmpty_M(&Arr.A[i]);
+    }
     return Arr;
 }
 
@@ -37,105 +41,20 @@ int GetCapacity_SB(ArrayDin_SB array)
     return array.Capacity;
 }
 
-void InsertAt_SB(ArrayDin_SB *array, ElType_SB el, IdxType i)
+void AddNeff_SB(ArrayDin_SB *array)
 {
     if (array->Neff == array->Capacity)
     {
         array->Capacity *= 2;
         array->A = (ElType_SB *) realloc (array->A, array->Capacity * sizeof(ElType_SB));
     }
-    IdxType temp = array->Neff;
-    while (temp > i)
-    {
-        array->A[temp] = array->A[temp - 1];
-        temp--;
-    }
-    array->A[i] = el;
+    CreateEmpty_M(&array->A[array->Neff]);
     array->Neff++;
 }
 
-void InsertLast_SB(ArrayDin_SB *array, ElType_SB el)
+void SubNeff_SB(ArrayDin_SB *array)
 {
-    InsertAt_SB(array, el, array->Neff);
-}
-
-void InsertFirst_SB(ArrayDin_SB *array, ElType_SB el)
-{
-    InsertAt_SB(array, el, 0);
-}
-
-void DeleteAt_SB(ArrayDin_SB *array, IdxType i)
-{
-    IdxType temp = i;
-    while (temp < array->Neff - 1)
-    {
-        array->A[temp] = array->A[temp + 1];
-        temp++;
-    }
-
-    if (array->Neff > 1)
-    {
-        array->Neff--;
-    }
-    else
-    {
-        array->Neff = 0;
-    }
-}
-
-void DeleteLast_SB(ArrayDin_SB *array)
-{
-    DeleteAt_SB(array, array->Neff - 1);
-}
-
-void DeleteFirst_SB(ArrayDin_SB *array)
-{
-    DeleteAt_SB(array, 0);
-}
-
-void PrintArrayDin_SB(ArrayDin_SB array)
-{
-    if (array.Neff == 0)
-    {
-        printf("[]\n");
-    }
-    else
-    {
-        IdxType i;
-        printf("[");
-        for (i = 0; i < array.Neff - 1; i++)
-        {
-            printf("%s, ", array.A[i]);
-        }
-        printf("%s]\n", array.A[array.Neff - 1]);
-    }
-}
-
-void ReverseArrayDin_SB(ArrayDin_SB *array)
-{
-    IdxType i = array->Neff - 1;
-    IdxType count = 0;
-    ElType_SB temp;
-    while (count < i)
-    {
-        temp = array->A[count];
-        array->A[count] = array->A[i];  
-        array->A[i] = temp;
-        count++;
-        i--;
-    }
-}
-
-ArrayDin_SB CopyArrayDin_SB(ArrayDin_SB array)
-{
-    ArrayDin_SB ArrCopy;
-    ArrCopy = MakeArrayDin_SB();
-    IdxType i;
-    for (i = 0; i < array.Neff; i++)
-    {
-        InsertLast_SB(&ArrCopy, array.A[i]);
-    }
-    return ArrCopy;
+    array->Neff--;
 }
 
 IdxType SearchArrayDin_SB(ArrayDin_SB array, ElType_SB el)
