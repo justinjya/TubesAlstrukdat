27b3fe91ce3ef78ce90e082a7f591378e5771c8f
justinjya <103380665+justinjya@users.noreply.github.com>
2022-12-01 11:51:08 +0700

    Update towerffhanoi.c

--- src/games/towerffhanoi.c
+++ src/games/towerffhanoi.c
@@ -231,7 +231,7 @@ int TOWEROFHANOI(){
     score = ((jumlahPiringan * 20) - (moves - (power(2, jumlahPiringan) - 1))) / 10; //Hitung skor akhir pemain
     printf("\n");
     printf("SELAMAT, KAMU BERHASIL!\n\n");
-    printf("Total move: %d\n", moves);
-    printf("Skor yang didapatkan: %d\n", score);
+    printf("Jumlah putaran: %d\n", moves);
+    printf("Skor akhir: %d\n", score);
     return score;
 }
