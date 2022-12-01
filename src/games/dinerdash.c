01b283b19ae48ec518887a1332a1b9dc22352768
justinjya <103380665+justinjya@users.noreply.github.com>
2022-12-01 13:31:49 +0700

    Update dinerdash.c

--- src/games/dinerdash.c
+++ src/games/dinerdash.c
@@ -110,7 +110,7 @@ int DINERDASH(){
     PushPesanan(&pesanan,CreatePesanan("M0",randomNumber(1,3),randomNumber(2,5),randomNumber(10,50) * 1000));
     PushPesanan(&pesanan,CreatePesanan("M1",randomNumber(1,3),randomNumber(2,5),randomNumber(10,50) * 1000));
     PushPesanan(&pesanan,CreatePesanan("M2",randomNumber(1,3),randomNumber(2,5),randomNumber(10,50) * 1000));
-    printf("\n==================================== DINER DASH ====================================\n");
+    printf("\n====================================== DINER DASH ======================================\n");
     printf("Selamat datang di Diner Dash!\n\n");
     boolean endGame = false;
     
@@ -321,6 +321,7 @@ int DINERDASH(){
             }
         }
     }
+    printf("\n===================================== GAME BERAKHIR ====================================\n");
     printf("Skor akhir: %d\n",saldo);
     return saldo;
 }
